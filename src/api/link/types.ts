d828cf9b38ef6e22b6ebcf85bc878d0eaab0c964
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2023-06-12 17:17:29 +0800

    types dirty fix

--- src/api/link/types.ts
+++ src/api/link/types.ts
@@ -1,48 +1,48 @@
 import { AxiosResponse } from 'axios'
 
 type TDepositResponse = {
-  success: boolean,
-  data: {
-    tx_hash: string
-  }
+    success: boolean,
+    data: {
+        tx_hash: string
+    }
 }
 
 type TDeposit = (
-  apiHost: string,
-  sender: string,
-  escrow: string,
-  transfer_id: string,
-  expiration: number,
-  amount: string,
-  authorization: string
+    apiHost: string,
+    sender: string,
+    escrow: string,
+    transfer_id: string,
+    expiration: string,
+    amount: string,
+    authorization: string
 ) => Promise<
-  AxiosResponse<
-    TDepositResponse
-  >
+AxiosResponse<
+TDepositResponse
+>
 >
 
 type TRedeemLinkResponse = {
-  success: boolean,
-  data: {
-    tx_hash: string
-  }
+    success: boolean,
+    data: {
+        tx_hash: string
+    }
 }
 
 type TRedeemLink = (
-  apiHost: string,
-  receiver: string,
-  sender: string,
-  escrow: string,
-  transfer_id: string,
-  receiver_sig: string,
-  sender_sig: string
+    apiHost: string,
+    receiver: string,
+    sender: string,
+    escrow: string,
+    transfer_id: string,
+    receiver_sig: string,
+    sender_sig: string
 ) => Promise<
-  AxiosResponse<
-    TRedeemLinkResponse
-  >
+AxiosResponse<
+TRedeemLinkResponse
+>
 >
 
 export type TRequests = {
-  redeemLink: TRedeemLink
-  deposit: TDeposit
+    redeemLink: TRedeemLink
+    deposit: TDeposit
 }
--- src/utils/get-deposit-authorization/index.ts
+++ src/utils/get-deposit-authorization/index.ts
@@ -3,50 +3,50 @@ import { getNonce } from '..'
 import { TDomain, TSignerCustomized } from '../../types'
 
 async function getDepositAuthorization(
-  signer: TSignerCustomized,
-  to: string,
-  amount: string,
-  validAfter: number,
-  validBefore: number,
-  transferId: string,
-  expiration: number,
-  domain: TDomain
+    signer: TSignerCustomized,
+    to: string,
+    amount: string,
+    validAfter: number,
+    validBefore: number,
+    transferId: string,
+    expiration: string,
+    domain: TDomain
 ) {
-  // The EIP-712 type data
-  const types = {
-    TransferWithAuthorization: [
-      { name: 'from', type: 'address' },
-      { name: 'to', type: 'address' },
-      { name: 'value', type: 'uint256' },
-      { name: 'validAfter', type: 'uint256' },
-      { name: 'validBefore', type: 'uint256' },
-      { name: 'nonce', type: 'bytes32' },
-    ],
-  }
-  const sender = await signer.getAddress()
-  const nonce = getNonce(sender, transferId, amount, expiration)
-  const message = {
-    from: sender,
-    to,
-    value: amount,
-    validAfter,
-    validBefore,
-    nonce    
-  }
+    // The EIP-712 type data
+    const types = {
+        TransferWithAuthorization: [
+            { name: 'from', type: 'address' },
+            { name: 'to', type: 'address' },
+            { name: 'value', type: 'uint256' },
+            { name: 'validAfter', type: 'uint256' },
+            { name: 'validBefore', type: 'uint256' },
+            { name: 'nonce', type: 'bytes32' },
+        ],
+    }
+    const sender = await signer.getAddress()
+    const nonce = getNonce(sender, transferId, amount, expiration)
+    const message = {
+        from: sender,
+        to,
+        value: amount,
+        validAfter,
+        validBefore,
+        nonce
+    }
 
-  if (signer._signTypedData) {
-    const signature = await signer._signTypedData(domain, types, message)
-    const signatureSplit = ethers.utils.splitSignature(signature)
+    if (signer._signTypedData) {
+        const signature = await signer._signTypedData(domain, types, message)
+        const signatureSplit = ethers.utils.splitSignature(signature)
 
-    // Encode the authorization
-    const authorization = ethers.utils.defaultAbiCoder.encode(
-      ['address', 'address', 'uint256', 'uint256', 'uint256', 'bytes32', 'uint8', 'bytes32', 'bytes32'],
-      [message.from, message.to, message.value, message.validAfter, message.validBefore, message.nonce, signatureSplit.v, signatureSplit.r, signatureSplit.s]
-    )
+        // Encode the authorization
+        const authorization = ethers.utils.defaultAbiCoder.encode(
+            ['address', 'address', 'uint256', 'uint256', 'uint256', 'bytes32', 'uint8', 'bytes32', 'bytes32'],
+            [message.from, message.to, message.value, message.validAfter, message.validBefore, message.nonce, signatureSplit.v, signatureSplit.r, signatureSplit.s]
+        )
 
-    return authorization
-  }
-  
+        return authorization
+    }
+
 }
 
 export default getDepositAuthorization
