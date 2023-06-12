53cc4d5e8b18c4a7466b553d56caff9120c4738e
spacehaz <spacehaz@gmail.com>
2023-06-12 15:58:48 +0300

    update for sdk params

--- src/modules/link-sdk/index.ts
+++ src/modules/link-sdk/index.ts
@@ -10,85 +10,96 @@ import { mumbaiApiUrl, polygonApiUrl } from '../../configs'
 import { decodeLink } from '../../helpers'
 
 class LinkSDK implements ILinkSDK {
-    apiKey: string
+  apiKey: string
+  signer: Signer
+  escrow: ethers.Contract
+  apiHost?: string
+
+  constructor({
+    apiKey,
+    apiHost,
+    signer
+  }: {
+    apiKey: string,
+    apiHost?: string,
     signer: Signer
-    escrow: ethers.Contract
-    apiHost: string
+  }) {
+    this.apiKey = apiKey
+    this.signer = signer
+    this.apiHost = apiHost
+    this.escrow = new ethers.Contract(escrowAddress, escrowABI, signer)
+  }
 
-    constructor(
-        apiKey: string,
-        apiHost: string,
-        signer: Signer
-    ) {
-        this.apiKey = apiKey
-        this.signer = signer
-        this.apiHost = apiHost
-        this.escrow = new ethers.Contract(escrowAddress, escrowABI, signer)
+  getApiHost: TGetApiHost = async () => {
+    if (this.apiHost) { return this.apiHost }
+    const chainId = await this.signer.getChainId()
+    if (chainId === 80001) {
+      return mumbaiApiUrl
+    } else if (chainId === 137) {
+      return polygonApiUrl
     }
+    throw new Error('Api host is not provided or chain_id is not appropriate for SDK. Use Polygon or Mumbai')
+  }
 
-    getApiHost: TGetApiHost = async () => {
-        if (this.apiHost) { return this.apiHost }
-        const chainId = await this.signer.getChainId()
-        if (chainId === 80001) {
-            return mumbaiApiUrl
-        } else if (chainId === 137) {
-            return polygonApiUrl
-        }
-        throw new Error('Api host is not provided or chain_id is not appropriate for SDK. Use Polygon or Mumbai')
+  getLinkdrop: TGetLinkdrop = async ({ token, transferId }) => {
+    const apiHost = await this.getApiHost()
+    const options = {
+      apiHost: apiHost,
+      signer: this.signer,
+      escrow: this.escrow,
     }
+    return new Linkdrop({ token, transferId, options })
+  }
 
-    getLinkdrop: TGetLinkdrop = ({ token, transferId }) => {
-        return new Linkdrop({ token, transferId })
+  createLinkdrop: TCreateLinkdrop = async ({ token, amount, expiration }) => {
+    const apiHost = await this.getApiHost()
+    const options = {
+      signer: this.signer,
+      escrow: this.escrow,
+      apiHost: apiHost
     }
+    const linkdrop = new Linkdrop({ token, amount, expiration, options })
+    await linkdrop.initialize()
+    return linkdrop
+  }
 
-    createLinkdrop: TCreateLinkdrop = async ({ token, amount, expiration }) => {
-        const apiHost = await this.getApiHost()
-        const options = {
-            signer: this.signer,
-            escrow: this.escrow,
-            apiHost: apiHost
-        }
-        const linkdrop = new Linkdrop({ token, amount, expiration, options })
-        await linkdrop.initialize()
-        return linkdrop
-    }
+  fetchHistory: TFetchHistory = async ({ sender, token }) => {
+    const linkdrops = await axios.get('')
+    return linkdrops
+  }
 
-    fetchHistory: TFetchHistory = async ({ sender, token }) => {
-        const linkdrops = await axios.get('')
-        return linkdrops
-    }
+  getCurrentFee: TGetCurrentFee = async (token) => {
+    return '0'
+  }
 
-    getCurrentFee: TGetCurrentFee = async (token) => {
-        return '0'
-    }
+  getDepositAmount: TGetDepositAmount = async (link) => {
+    const decodedLinkParams = this._parseUrl(link)
+    const { transferId, sender } = decodedLinkParams
+    const { amount, expiration } = await this.escrow.getDeposit(sender, transferId)
+    return { amount, expiration }
+  }
 
-    getDepositAmount: TGetDepositAmount = async (link) => {
-        const transferId = "1345678"
-        const sender = "0x4D0714544Ede1BE9bc39d73846B0fF2233DE79c8"
-        const { amount, expiration } = await this.escrow.getDeposit(sender, transferId)
-        return amount
-    }
+  redeem: TRedeem = async (link, to) => {
+      const decodedLinkParams = this._parseUrl(link)
+      const { senderSig, linkKey, transferId, sender } = decodedLinkParams
+      const receiverSig = await generateReceiverSig(linkKey, to)
+      const apiHost = await this.getApiHost()
+      const redeem = await linkApi.redeemLink(
+          apiHost,
+          to,
+          sender,
+          this.escrow.address,
+          transferId,
+          receiverSig,
+          senderSig
+      )
+      const { data } = redeem
+      return data
+  }
 
-    redeem: TRedeem = async (link, to) => {
-        const decodedLinkParams = this._parseUrl(link)
-        const { senderSig, linkKey, transferId, sender } = decodedLinkParams
-        const receiverSig = await generateReceiverSig(linkKey, to)
-        const redeem = await linkApi.redeemLink(
-            this.apiHost,
-            to,
-            sender,
-            this.escrow.address,
-            transferId,
-            receiverSig,
-            senderSig
-        )
-        const { data } = redeem
-        return data
-    }
-
-    _parseUrl: TParseURL = (link) => {
-        return decodeLink(link)
-    }
+  _parseUrl: TParseURL = (link) => {
+      return decodeLink(link)
+  }
 }
 
 export default LinkSDK
--- src/utils/get-deposit-authorization/index.ts
+++ src/utils/get-deposit-authorization/index.ts
@@ -14,37 +14,37 @@ async function getDepositAuthorization(
 ) {
     // The EIP-712 type data
     const types = {
-        TransferWithAuthorization: [
-            { name: 'from', type: 'address' },
-            { name: 'to', type: 'address' },
-            { name: 'value', type: 'uint256' },
-            { name: 'validAfter', type: 'uint256' },
-            { name: 'validBefore', type: 'uint256' },
-            { name: 'nonce', type: 'bytes32' },
-        ],
+      TransferWithAuthorization: [
+        { name: 'from', type: 'address' },
+        { name: 'to', type: 'address' },
+        { name: 'value', type: 'uint256' },
+        { name: 'validAfter', type: 'uint256' },
+        { name: 'validBefore', type: 'uint256' },
+        { name: 'nonce', type: 'bytes32' },
+      ],
     }
     const sender = await signer.getAddress()
     const nonce = getNonce(sender, transferId, amount, expiration)
     const message = {
-        from: sender,
-        to,
-        value: amount,
-        validAfter,
-        validBefore,
-        nonce
+      from: sender,
+      to,
+      value: amount,
+      validAfter,
+      validBefore,
+      nonce
     }
 
     if (signer._signTypedData) {
-        const signature = await signer._signTypedData(domain, types, message)
-        const signatureSplit = ethers.utils.splitSignature(signature)
+      const signature = await signer._signTypedData(domain, types, message)
+      const signatureSplit = ethers.utils.splitSignature(signature)
 
-        // Encode the authorization
-        const authorization = ethers.utils.defaultAbiCoder.encode(
-            ['address', 'address', 'uint256', 'uint256', 'uint256', 'bytes32', 'uint8', 'bytes32', 'bytes32'],
-            [message.from, message.to, message.value, message.validAfter, message.validBefore, message.nonce, signatureSplit.v, signatureSplit.r, signatureSplit.s]
-        )
+      // Encode the authorization
+      const authorization = ethers.utils.defaultAbiCoder.encode(
+        ['address', 'address', 'uint256', 'uint256', 'uint256', 'bytes32', 'uint8', 'bytes32', 'bytes32'],
+        [message.from, message.to, message.value, message.validAfter, message.validBefore, message.nonce, signatureSplit.v, signatureSplit.r, signatureSplit.s]
+      )
 
-        return authorization
+      return authorization
     }
 
 }
