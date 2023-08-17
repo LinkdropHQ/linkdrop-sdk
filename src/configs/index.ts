f09be7d3397d0b70d888e1bfc429db6d706f9113
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2023-08-17 11:22:51 +0800

    mumbai escrow address updated

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -7,7 +7,7 @@ export const mainnetJSONRPCUrl = 'https://mainnet.infura.io/v3/620c738fbe1843a18
 export const goerliJSONRPCUrl = 'https://goeli.infura.io/v3/620c738fbe1843a18f47ada0e60e738a'
 export const mumbaiJSONRPCUrl = 'https://rpc-mumbai.maticvigil.com/v1/f592ae2e5afb3bebe39314e9bd0949de5b74cd2f'
 
-export const mumbaiEscrowAddress = "0x89C0123826AD31f0BcE61d6f28Bd2175F46e8b74"
+export const mumbaiEscrowAddress = "0x85dF066235BC89D25d78FF5e98cbCc5Fc91e7e05"
 export const polygonEscrowAddress = "0x3013e9c3a86c23f74c9d885f070e28d69b1e09f5"
 
 export const claimHost = 'http://localhost:3000'
