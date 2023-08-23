98afeefa0641868fb2fa85fa28479139550b583f
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2023-08-23 16:05:55 +0900

    k param deconstructed to k (linkKey), s (senderSignature) & i (transferId)

--- src/helpers/encode-link.ts
+++ src/helpers/encode-link.ts
@@ -3,11 +3,11 @@ import { TLink } from "../types"
 
 type TEncodeLink = (claimHost: string, link: TLink) => string
 const encodeLink: TEncodeLink = (claimHost, link) => {
-    const linkKey = ethers.utils.base58.encode(link.linkKey)
-    const sig = ethers.utils.base58.encode(link.senderSig)
-    const transferId = ethers.utils.base58.encode(ethers.BigNumber.from(link.transferId).toHexString()) // string -> hex -> base58 for shorter strng
-    const k = [linkKey, sig, transferId].join("") // linkKey is always first 44 symbols, signature is the next 89 symbols and rest symbols are transferid
-    return `${claimHost}/#/usdc?k=${k}&c=${link.chainId}`
+  const linkKey = ethers.utils.base58.encode(link.linkKey)
+  const sig = ethers.utils.base58.encode(link.senderSig)
+  const transferId = ethers.utils.base58.encode(ethers.BigNumber.from(link.transferId).toHexString()) // string -> hex -> base58 for shorter string
+
+  return `${claimHost}/#/usdc?k=${linkKey}&s=${sig}&i=${transferId}&c=${link.chainId}`
 }
 
 export default encodeLink
