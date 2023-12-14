59c4f8e69f7c895bce6639042562e6db4b6d32d6
spacehaz <spacehaz@gmail.com>
2023-12-14 11:33:15 +0100

    added new escrow addresses

--- src/types/escrow-address/index.ts
+++ src/types/escrow-address/index.ts
@@ -1,6 +1,6 @@
 export enum EEscrowAddress {
-  polygon = '0xc4eb6e5933bc5e32dfd5c80baf143212a95549b3',
-  base = '0xad27383460183fd7e21b71df3b4cac9480eb9a75',
-  baseGoerli = '0xad27383460183fd7e21b71df3b4cac9480eb9a75',
+  polygon = '0x2d5dfe0e4582c905233df527242616017f36e192',
+  base = '0x2d5dfe0e4582c905233df527242616017f36e192',
+  baseGoerli = '0x2d5dfe0e4582c905233df527242616017f36e192',
   mumbai = '0x0b962bbbf101941d0d0ec1041d01668dac36647a'
 }
