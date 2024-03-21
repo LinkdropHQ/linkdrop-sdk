0e6dd564caa566e1ec56a4afd50890a6bd9e53de
spacehaz <spacehaz@gmail.com>
2024-03-21 10:55:01 +0300

    added parsing for dashboard links in new format

--- src/helpers/get-claim-code-from-dashboard-link.ts
+++ src/helpers/get-claim-code-from-dashboard-link.ts
@@ -2,11 +2,19 @@ import { parseQueryParams } from '.'
 import { TClaimLinkSource } from '../types'
 
 type TGetClaimCodeFromDashboardLink = (claimUrl: string) => string
+// https://wallet.coinbase.com/claim?tk=code&k=ep1XdTfKdxGH&c=8453&v=3&src=d
 
 const getClaimCodeFromDashboardLink: TGetClaimCodeFromDashboardLink = (claimUrl) => {
-  const linkParts = claimUrl.split('/')
-  const claimCode = linkParts[linkParts.length - 1].split('?')[0]
-  return claimCode
+  if (claimUrl.includes('redeem')) {
+    const linkParts = claimUrl.split('/')
+    const claimCode = linkParts[linkParts.length - 1].split('?')[0]
+    return claimCode
+  } else {
+    const paramsString = claimUrl.split('?')[1]
+    const parsedParams = parseQueryParams(paramsString)
+    const claimCode = parsedParams["k"]
+    return claimCode
+  }
 }
 
 export default getClaimCodeFromDashboardLink
--- src/helpers/get-link-source-from-claim-url.ts
+++ src/helpers/get-link-source-from-claim-url.ts
@@ -4,8 +4,7 @@ import { TClaimLinkSource } from '../types'
 type TGetLinkSourceFromClaimUrl = (claimUrl: string) => TClaimLinkSource
 
 const getLinkSourceFromClaimUrl: TGetLinkSourceFromClaimUrl = (claimUrl) => {
-  const hashIndex = claimUrl.indexOf('#');
-  const paramsString = claimUrl.substring(hashIndex + 1).split('?')[1]
+  const paramsString = claimUrl.split('?')[1]
   const parsedParams = parseQueryParams(paramsString)
   const linkSource = parsedParams["src"]
   if (!linkSource) {
