1554ec03758092bac6c66eab582e2e901f16acf5
spacehaz <spacehaz@gmail.com>
2024-07-04 14:01:14 +0300

    added updates for hex length

--- src/helpers/decode-link.ts
+++ src/helpers/decode-link.ts
@@ -3,6 +3,15 @@ import { parseQueryParams } from '.'
 import { ethers, decodeBase58 } from 'ethers'
 type TDecodeLink = (link: string) => TLink
 
+const defineSig = (
+  signatureLength: number = 132,
+  signature?: string
+) => {
+  if (!signature) return undefined
+  const originalSignature = decodeBase58(signature)
+  return ethers.toBeHex(originalSignature, (signatureLength - 2) / 2)
+}
+
 const decodeLink: TDecodeLink = (link) => {
   const hashIndex = link.indexOf('#');
   const paramsString = link.substring(hashIndex + 1).split('?')[1]
@@ -13,11 +22,14 @@ const decodeLink: TDecodeLink = (link) => {
     signature: parsedParams["sg"],
     transferId: parsedParams["i"] || "",
     chainId: parsedParams["c"],
-    version: parsedParams["v"] || "1"
+    version: parsedParams["v"] || "1",
+    signatureLength: parsedParams["sgl"] || '132'
   }
 
-  const linkKey = ethers.toBeHex(decodeBase58(params.linkKey), 32)
-  const senderSig = params.signature ? ethers.toBeHex(decodeBase58(params.signature), 65) : undefined
+  const linkKey = ethers.toBeHex(decodeBase58(params.linkKey), 32) 
+
+  const senderSig = defineSig(Number(params.signatureLength), params.signature)
+
   const transferId = params.transferId ? ethers.toBeHex(decodeBase58(params.transferId), 20) : (new ethers.Wallet(linkKey)).address.toLowerCase()
 
   const chainId = Number(params.chainId)
