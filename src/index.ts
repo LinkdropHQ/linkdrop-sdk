bcc7cb4720de3fd36cd975a97cceca84fcf1fad1
spacehaz <spacehaz@gmail.com>
2024-07-30 20:09:12 +0300

    support for v3 links

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,16 @@ import {
   ClaimLink as ClaimLinkV2
 } from 'linkdrop-p2p-sdk2'
 
+import {
+  LinkdropP2P as LinkdropP2PV3_11,
+  ClaimLink as ClaimLinkV3_11
+} from 'linkdrop-p2p-sdk3.11.0-beta'
+
 export {
   LinkdropP2P,
   ClaimLink,
   LinkdropP2PV2,
-  ClaimLinkV2
+  ClaimLinkV2,
+  LinkdropP2PV3_11,
+  ClaimLinkV3_11
 }
