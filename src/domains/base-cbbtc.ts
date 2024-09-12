ee8a24acc96ea9536e3361621656e9b07f850300
spacehaz <spacehaz@gmail.com>
2024-09-12 13:12:16 +0300

    added cbBTC token for deposit with auth

--- src/domains/base-cbbtc.ts
+++ src/domains/base-cbbtc.ts
@@ -1,10 +1,10 @@
 import { EChains, ETokenAddress } from "../types"
 
-const baseEURC = {
+const baseCBBTC = {
   name: 'Coinbase Wrapped BTC',
   version: '2',
   chainId: EChains.base,
   verifyingContract: ETokenAddress.cbBTC
 }
 
-export default baseEURC
+export default baseCBBTC
