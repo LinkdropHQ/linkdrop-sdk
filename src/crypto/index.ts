33bbfe86bf59ccd811aea9fbf911583cc91aa531
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2024-12-16 15:28:51 +0300

    encryption/decryption deps updated to support react-native

--- src/crypto/index.ts
+++ src/crypto/index.ts
@@ -1,9 +1,12 @@
-import { ChaCha20Poly1305 } from "@stablelib/chacha20poly1305"
-import { concat, fromString, toString } from "uint8arrays"
+import nacl from "tweetnacl";
+import base64js from "base64-js";
+
+// Polyfill TextEncoder and TextDecoder for React Native
+import { TextEncoder, TextDecoder } from 'text-encoding';
 
 export const BASE10 = "base10";
-export const BASE16 = "base16";
-export const BASE64 = "base64pad";
+export const BASE16 = "hex";
+export const BASE64 = "base64";
 export const BASE64URL = "base64url";
 export const UTF8 = "utf8";
 export const TYPE_0 = 0;
@@ -11,71 +14,119 @@ export const TYPE_1 = 1;
 export const TYPE_2 = 2;
 const ZERO_INDEX = 0;
 const TYPE_LENGTH = 1;
-const IV_LENGTH = 12;
+const IV_LENGTH = 24;
 const KEY_LENGTH = 32;
 
-export function encodeTypeByte(type: number): Uint8Array {
-  return fromString(`${type}`, BASE10);
+function toUtf8(string) {
+  const encoder = new TextEncoder();
+  return encoder.encode(string);
 }
 
-export function decodeTypeByte(byte: Uint8Array): number {
-  return Number(toString(byte, BASE10));
+function fromUtf8(bytes) {
+  const decoder = new TextDecoder();
+  return decoder.decode(bytes);
 }
 
-export function decrypt(params: any): string {
-  const box = new ChaCha20Poly1305(fromString(params.symKey, BASE16));
+function toHex(uint8Array) {
+  return Array.from(uint8Array).map((byte: number) => byte.toString(16).padStart(2, '0')).join('');
+}
+
+function fromHex(hex) {
+  const len = hex.length;
+  const arr = new Uint8Array(len / 2);
+  for (let i = 0; i < len; i += 2) {
+    arr[i / 2] = parseInt(hex.substr(i, 2), 16);
+  }
+  return arr;
+}
+
+function toBase64(uint8Array) {
+  return base64js.fromByteArray(uint8Array);
+}
+
+function fromBase64(base64) {
+  return base64js.toByteArray(base64);
+
+}
+
+export function encodeTypeByte(type) {
+  return toUtf8(`${type}`);
+}
+
+export function decodeTypeByte(byte) {
+  return Number(fromUtf8(byte));
+}
+
+export function decrypt(params) {
+  const key = fromHex(params.symKey);
   const { sealed, iv } = deserialize({
     encoded: params.encoded,
     randomBytes: params.randomBytes,
-    encoding: params?.encoding
+    encoding: params?.encoding,
   });
-  const message = box.open(iv, sealed);
-  if (message === null) throw new Error("Failed to decrypt");
-  return toString(message, UTF8);
+
+  const message = nacl.secretbox.open(sealed, iv, key);
+  if (!message) throw new Error("Failed to decrypt");
+  return fromUtf8(message);
 }
 
-
-export function encrypt(params: any): string {
-  const type = encodeTypeByte(typeof params.type !== "undefined" ? params.type : TYPE_0);
+export function encrypt(params) {
+  console.log("SDK: in encrypt...")
+  console.log("SDK:", params)
+  const type = encodeTypeByte(
+    typeof params.type !== "undefined" ? params.type : TYPE_0
+  );
   if (decodeTypeByte(type) === TYPE_1 && typeof params.senderPublicKey === "undefined") {
     throw new Error("Missing sender public key for type 1 envelope");
   }
+
   const senderPublicKey =
     typeof params.senderPublicKey !== "undefined"
-      ? fromString(params.senderPublicKey, BASE16)
+      ? fromHex(params.senderPublicKey)
       : undefined;
 
   const iv =
-    typeof params.iv !== "undefined" ? fromString(params.iv, BASE16) : params.randomBytes(IV_LENGTH);
-  const box = new ChaCha20Poly1305(fromString(params.symKey, BASE16));
-  const sealed = box.seal(iv, fromString(params.message, UTF8));
+    typeof params.iv !== "undefined" ? fromHex(params.iv) : params.randomBytes(IV_LENGTH);
+  const key = fromHex(params.symKey);
+  console.log("SDK: before sealed")
+  console.log("SDK: ", { key, iv, message: toUtf8(params.message) })
+  const sealed = nacl.secretbox(toUtf8(params.message), iv, key);
+
+  console.log("SDK: SEALED!")
+  console.log({ sealed })
+
   return serialize({ type, sealed, iv, senderPublicKey, encoding: params.encoding });
 }
 
-export function serialize(params: any): string {
+export function serialize(params) {
   const { encoding = BASE64 } = params;
 
   if (decodeTypeByte(params.type) === TYPE_2) {
-    return toString(concat([params.type, params.sealed]), encoding);
+    return toBase64(new Uint8Array([...params.type, ...params.sealed]));
   }
   if (decodeTypeByte(params.type) === TYPE_1) {
     if (typeof params.senderPublicKey === "undefined") {
       throw new Error("Missing sender public key for type 1 envelope");
     }
-    return toString(
-      concat([params.type, params.senderPublicKey, params.iv, params.sealed]),
-      encoding,
+    return toBase64(
+      new Uint8Array([
+        ...params.type,
+        ...params.senderPublicKey,
+        ...params.iv,
+        ...params.sealed,
+      ])
     );
   }
   // default to type 0 envelope
-  return toString(concat([params.type, params.iv, params.sealed]), encoding);
+  return toBase64(new Uint8Array([...params.type, ...params.iv, ...params.sealed]));
 }
 
-export function deserialize(params: any): any {
+export function deserialize(params) {
   const { encoded, encoding = BASE64 } = params;
-  const bytes = fromString(encoded, encoding);
+  const bytes = fromBase64(encoded);
   const type = bytes.slice(ZERO_INDEX, TYPE_LENGTH);
   const slice1 = TYPE_LENGTH;
+
   if (decodeTypeByte(type) === TYPE_1) {
     const slice2 = slice1 + KEY_LENGTH;
     const slice3 = slice2 + IV_LENGTH;
